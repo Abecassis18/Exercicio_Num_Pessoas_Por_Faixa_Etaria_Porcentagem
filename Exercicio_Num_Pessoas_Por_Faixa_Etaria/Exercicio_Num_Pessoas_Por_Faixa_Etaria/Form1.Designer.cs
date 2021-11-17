f0aabcfe42bd316ee5a33dcdfd2e6a22352c3c8b
Abecassis18 <abecassis_mp@outlook.com>
2021-11-17 21:14:24 +0000

    Upload do exercicio mas para revião mais tarde, para o calculo de Porcentagem

---
+++ Exercicio_Num_Pessoas_Por_Faixa_Etaria/Exercicio_Num_Pessoas_Por_Faixa_Etaria/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿
+namespace Exercicio_Num_Pessoas_Por_Faixa_Etaria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lbl1a15 = new System.Windows.Forms.Label();
+            this.btmAdicionar = new System.Windows.Forms.Button();
+            this.lbl16a30 = new System.Windows.Forms.Label();
+            this.lbl31a45 = new System.Windows.Forms.Label();
+            this.lbl46a60 = new System.Windows.Forms.Label();
+            this.lbl60mais = new System.Windows.Forms.Label();
+            this.lbl60maispor = new System.Windows.Forms.Label();
+            this.lbl46a60por = new System.Windows.Forms.Label();
+            this.lbl31a45por = new System.Windows.Forms.Label();
+            this.lbl16a30por = new System.Windows.Forms.Label();
+            this.lbl1a15por = new System.Windows.Forms.Label();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.contadorTotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite a idade";
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.Location = new System.Drawing.Point(119, 26);
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.Size = new System.Drawing.Size(100, 20);
+            this.txtIdade.TabIndex = 1;
+            this.txtIdade.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "1 a 15";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(77, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "16 a 30";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(77, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "31 a 45";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(77, 218);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "46 a 60";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(77, 252);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Mais de 60";
+            // 
+            // lbl1a15
+            // 
+            this.lbl1a15.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl1a15.Location = new System.Drawing.Point(187, 102);
+            this.lbl1a15.Name = "lbl1a15";
+            this.lbl1a15.Size = new System.Drawing.Size(48, 22);
+            this.lbl1a15.TabIndex = 7;
+            this.lbl1a15.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btmAdicionar
+            // 
+            this.btmAdicionar.Location = new System.Drawing.Point(241, 24);
+            this.btmAdicionar.Name = "btmAdicionar";
+            this.btmAdicionar.Size = new System.Drawing.Size(105, 23);
+            this.btmAdicionar.TabIndex = 12;
+            this.btmAdicionar.Text = "Adicionar Idade";
+            this.btmAdicionar.UseVisualStyleBackColor = true;
+            this.btmAdicionar.Click += new System.EventHandler(this.btmAdicionar_Click);
+            // 
+            // lbl16a30
+            // 
+            this.lbl16a30.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl16a30.Location = new System.Drawing.Point(187, 137);
+            this.lbl16a30.Name = "lbl16a30";
+            this.lbl16a30.Size = new System.Drawing.Size(48, 22);
+            this.lbl16a30.TabIndex = 13;
+            this.lbl16a30.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl31a45
+            // 
+            this.lbl31a45.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl31a45.Location = new System.Drawing.Point(187, 177);
+            this.lbl31a45.Name = "lbl31a45";
+            this.lbl31a45.Size = new System.Drawing.Size(48, 22);
+            this.lbl31a45.TabIndex = 14;
+            this.lbl31a45.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl46a60
+            // 
+            this.lbl46a60.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl46a60.Location = new System.Drawing.Point(187, 213);
+            this.lbl46a60.Name = "lbl46a60";
+            this.lbl46a60.Size = new System.Drawing.Size(48, 22);
+            this.lbl46a60.TabIndex = 15;
+            this.lbl46a60.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl60mais
+            // 
+            this.lbl60mais.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl60mais.Location = new System.Drawing.Point(187, 247);
+            this.lbl60mais.Name = "lbl60mais";
+            this.lbl60mais.Size = new System.Drawing.Size(48, 22);
+            this.lbl60mais.TabIndex = 16;
+            this.lbl60mais.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl60maispor
+            // 
+            this.lbl60maispor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl60maispor.Location = new System.Drawing.Point(264, 247);
+            this.lbl60maispor.Name = "lbl60maispor";
+            this.lbl60maispor.Size = new System.Drawing.Size(48, 22);
+            this.lbl60maispor.TabIndex = 21;
+            this.lbl60maispor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl46a60por
+            // 
+            this.lbl46a60por.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl46a60por.Location = new System.Drawing.Point(264, 213);
+            this.lbl46a60por.Name = "lbl46a60por";
+            this.lbl46a60por.Size = new System.Drawing.Size(48, 22);
+            this.lbl46a60por.TabIndex = 20;
+            this.lbl46a60por.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl31a45por
+            // 
+            this.lbl31a45por.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl31a45por.Location = new System.Drawing.Point(264, 177);
+            this.lbl31a45por.Name = "lbl31a45por";
+            this.lbl31a45por.Size = new System.Drawing.Size(48, 22);
+            this.lbl31a45por.TabIndex = 19;
+            this.lbl31a45por.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl16a30por
+            // 
+            this.lbl16a30por.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl16a30por.Location = new System.Drawing.Point(264, 137);
+            this.lbl16a30por.Name = "lbl16a30por";
+            this.lbl16a30por.Size = new System.Drawing.Size(48, 22);
+            this.lbl16a30por.TabIndex = 18;
+            this.lbl16a30por.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl1a15por
+            // 
+            this.lbl1a15por.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl1a15por.Location = new System.Drawing.Point(264, 102);
+            this.lbl1a15por.Name = "lbl1a15por";
+            this.lbl1a15por.Size = new System.Drawing.Size(48, 22);
+            this.lbl1a15por.TabIndex = 17;
+            this.lbl1a15por.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(241, 53);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(105, 23);
+            this.btnFechar.TabIndex = 22;
+            this.btnFechar.Text = "Fechar App";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(281, 84);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(15, 13);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "%";
+            // 
+            // contadorTotal
+            // 
+            this.contadorTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.contadorTotal.Location = new System.Drawing.Point(12, 75);
+            this.contadorTotal.Name = "contadorTotal";
+            this.contadorTotal.Size = new System.Drawing.Size(48, 22);
+            this.contadorTotal.TabIndex = 24;
+            this.contadorTotal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 286);
+            this.Controls.Add(this.contadorTotal);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.lbl60maispor);
+            this.Controls.Add(this.lbl46a60por);
+            this.Controls.Add(this.lbl31a45por);
+            this.Controls.Add(this.lbl16a30por);
+            this.Controls.Add(this.lbl1a15por);
+            this.Controls.Add(this.lbl60mais);
+            this.Controls.Add(this.lbl46a60);
+            this.Controls.Add(this.lbl31a45);
+            this.Controls.Add(this.lbl16a30);
+            this.Controls.Add(this.btmAdicionar);
+            this.Controls.Add(this.lbl1a15);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIdade);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Idades por faixa etária";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIdade;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lbl1a15;
+        private System.Windows.Forms.Button btmAdicionar;
+        private System.Windows.Forms.Label lbl16a30;
+        private System.Windows.Forms.Label lbl31a45;
+        private System.Windows.Forms.Label lbl46a60;
+        private System.Windows.Forms.Label lbl60mais;
+        private System.Windows.Forms.Label lbl60maispor;
+        private System.Windows.Forms.Label lbl46a60por;
+        private System.Windows.Forms.Label lbl31a45por;
+        private System.Windows.Forms.Label lbl16a30por;
+        private System.Windows.Forms.Label lbl1a15por;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label contadorTotal;
+    }
+}
+
---
+++ Exercicio_Num_Pessoas_Por_Faixa_Etaria/Exercicio_Num_Pessoas_Por_Faixa_Etaria/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercicio_Num_Pessoas_Por_Faixa_Etaria
+{
+    public partial class Form1 : Form
+    {
+        int contadorPessoas, ctdidade1, ctdidade2, ctdidade3, ctdidade4, ctdidade5, ctdidade6; //A principio quando tentei inicialia-las deu erro, então deixei assim e o Programa funciona.
+        double resultPorc = 0;
+     
+        public Form1()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = true;
+            notifyIcon1.BalloonTipText = "Em Execução";
+            notifyIcon1.ShowBalloonTip(6000);
+        }
+
+        private void btmAdicionar_Click(object sender, EventArgs e)
+        {
+            if(txtIdade.Text != "")
+            {
+                if (int.Parse(txtIdade.Text) > 0 && int.Parse(txtIdade.Text) <=15)
+                {
+                    ++ctdidade1;
+                    ++contadorPessoas;
+                    resultPorc = (double)ctdidade1 * ((double)contadorPessoas / 100);//Podemos evitar esta linha fazendo o calculo
+                                                                                    //no lbl1a15por.
+
+                    lbl1a15.Text = ctdidade1.ToString();
+                    lbl1a15por.Text = resultPorc.ToString();
+                }
+               else if(int.Parse(txtIdade.Text) > 15 && int.Parse(txtIdade.Text) <= 30)
+                {
+                    ++ctdidade2;
+                    ++contadorPessoas;
+
+                    resultPorc = (double)ctdidade2 * ((double)contadorPessoas / 100);
+
+                    lbl16a30.Text = ctdidade2.ToString();
+                    lbl16a30por.Text = resultPorc.ToString();                    
+                }
+                //contadorTotal.Text = contadorPessoas.ToString(); para ver se estava a a contar certo
+                else if(int.Parse(txtIdade.Text) > 30 && int.Parse(txtIdade.Text) <= 45)
+                {
+                    contadorPessoas++;
+                    ctdidade3++;
+
+                    resultPorc = (double)ctdidade3 * ((double)contadorPessoas / 100);
+
+                    lbl31a45.Text = ctdidade3.ToString();
+                    lbl31a45por.Text = resultPorc.ToString();
+                }
+
+               else if (int.Parse(txtIdade.Text) > 45 && int.Parse(txtIdade.Text) <= 60)
+                {
+                    contadorPessoas++;
+                    ctdidade4++;
+
+                    resultPorc = (double)ctdidade4 * ((double)contadorPessoas / 100);
+
+                    lbl46a60.Text = ctdidade4.ToString();
+                    lbl46a60por.Text = resultPorc.ToString();
+                }
+
+                else if (int.Parse(txtIdade.Text) > 60)
+                {
+                    contadorPessoas++;
+                    ctdidade5++;
+
+                    resultPorc = (double)ctdidade5 * ((double)contadorPessoas / 100);
+
+                    lbl60mais.Text = ctdidade5.ToString();
+                    lbl60maispor.Text = resultPorc.ToString();
+                }
+                else
+                    MessageBox.Show("Número inválido", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        
+            
+        } 
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
