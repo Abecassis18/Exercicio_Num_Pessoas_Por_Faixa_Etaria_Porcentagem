144552cfc2837d8b49f4f75eed03479980f514d2
Abecassis18 <abecassis_mp@outlook.com>
2021-11-18 23:06:24 +0000

    Atualização do código comparado com o do livro, resultado igual

--- Exercicio_Num_Pessoas_Por_Faixa_Etaria/Exercicio_Num_Pessoas_Por_Faixa_Etaria/Form1.cs
+++ Exercicio_Num_Pessoas_Por_Faixa_Etaria/Exercicio_Num_Pessoas_Por_Faixa_Etaria/Form1.cs
@@ -12,7 +12,7 @@ namespace Exercicio_Num_Pessoas_Por_Faixa_Etaria
 {
     public partial class Form1 : Form
     {
-        int contadorPessoas, ctdidade1, ctdidade2, ctdidade3, ctdidade4, ctdidade5, ctdidade6; //A principio quando tentei inicialia-las deu erro, então deixei assim e o Programa funciona.
+        int contadorPessoas, ctdidade1, ctdidade2, ctdidade3, ctdidade4, ctdidade5; //A principio quando tentei inicialia-las deu erro, então deixei assim e o Programa funciona.
         double resultPorc = 0;
      
         public Form1()
@@ -32,66 +32,98 @@ namespace Exercicio_Num_Pessoas_Por_Faixa_Etaria
         {
             if(txtIdade.Text != "")
             {
-                if (int.Parse(txtIdade.Text) > 0 && int.Parse(txtIdade.Text) <=15)
+                // Eis um modo alternativo e mais economico de cód. tbm pode ser feito dessa maneira: Habilitei novamente a parte abaixo
+                //pois preciso dela para o tratamento da letra no lugar de um número, caso o user digite errado.
+                    int numero = 0;
+                    bool validar = int.TryParse(txtIdade.Text, out numero);
+
+                //    if (validar == true && numero != 0)
+                //    {
+                //        if (numero > 0 && numero <= 15) ctdidade1++;
+                //        else if (numero <= 30) ctdidade2++;
+                //        else if (numero <= 45) ctdidade3++;
+                //        else if (numero <= 60) ctdidade4++;
+                //        else if (numero > 60) ctdidade5++;
+
+                //        contadorPessoas++;
+
+                //        lbl1a15.Text = ctdidade1.ToString();
+                //        lbl1a15por.Text = ((double)ctdidade1 * ((double)contadorPessoas)/100).ToString("P2");
+                //    }
+                //    else
+                //        MessageBox.Show("Insira um valor válido.", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //}
+                //else
+                //    MessageBox.Show("Insira um valor.", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                //{
+                if (validar == true)
                 {
-                    ++ctdidade1;
-                    ++contadorPessoas;
-                    resultPorc = (double)ctdidade1 * ((double)contadorPessoas / 100);//Podemos evitar esta linha fazendo o calculo
-                                                                                    //no lbl1a15por.
+                    if (int.Parse(txtIdade.Text) > 0 && int.Parse(txtIdade.Text) <= 15)
+                    {
+                        ++ctdidade1;
+                        ++contadorPessoas;
+                        resultPorc = (double)ctdidade1 * ((double)contadorPessoas / 100);//Podemos evitar esta linha fazendo o calculo
+                                                                                         //no lbl1a15por.
 
-                    lbl1a15.Text = ctdidade1.ToString();
-                    lbl1a15por.Text = resultPorc.ToString();
-                }
-               else if(int.Parse(txtIdade.Text) > 15 && int.Parse(txtIdade.Text) <= 30)
-                {
-                    ++ctdidade2;
-                    ++contadorPessoas;
+                        lbl1a15.Text = ctdidade1.ToString();
+                        lbl1a15por.Text = resultPorc.ToString("P2");
+                    }
+                    else if (int.Parse(txtIdade.Text) > 15 && int.Parse(txtIdade.Text) <= 30)
+                    {
+                        ++ctdidade2;
+                        ++contadorPessoas;
 
-                    resultPorc = (double)ctdidade2 * ((double)contadorPessoas / 100);
+                        resultPorc = (double)ctdidade2 * ((double)contadorPessoas / 100);
 
-                    lbl16a30.Text = ctdidade2.ToString();
-                    lbl16a30por.Text = resultPorc.ToString();                    
-                }
-                //contadorTotal.Text = contadorPessoas.ToString(); para ver se estava a a contar certo
-                else if(int.Parse(txtIdade.Text) > 30 && int.Parse(txtIdade.Text) <= 45)
-                {
-                    contadorPessoas++;
-                    ctdidade3++;
+                        lbl16a30.Text = ctdidade2.ToString();
+                        lbl16a30por.Text = resultPorc.ToString();
+                    }
+                    //contadorTotal.Text = contadorPessoas.ToString(); para ver se estava a a contar certo
+                    else if (int.Parse(txtIdade.Text) > 30 && int.Parse(txtIdade.Text) <= 45)
+                    {
+                        contadorPessoas++;
+                        ctdidade3++;
 
-                    resultPorc = (double)ctdidade3 * ((double)contadorPessoas / 100);
+                        resultPorc = (double)ctdidade3 * ((double)contadorPessoas / 100);
 
-                    lbl31a45.Text = ctdidade3.ToString();
-                    lbl31a45por.Text = resultPorc.ToString();
-                }
+                        lbl31a45.Text = ctdidade3.ToString();
+                        lbl31a45por.Text = resultPorc.ToString();
+                    }
 
-               else if (int.Parse(txtIdade.Text) > 45 && int.Parse(txtIdade.Text) <= 60)
-                {
-                    contadorPessoas++;
-                    ctdidade4++;
+                    else if (int.Parse(txtIdade.Text) > 45 && int.Parse(txtIdade.Text) <= 60)
+                    {
+                        contadorPessoas++;
+                        ctdidade4++;
 
-                    resultPorc = (double)ctdidade4 * ((double)contadorPessoas / 100);
+                        resultPorc = (double)ctdidade4 * ((double)contadorPessoas / 100);
 
-                    lbl46a60.Text = ctdidade4.ToString();
-                    lbl46a60por.Text = resultPorc.ToString();
-                }
+                        lbl46a60.Text = ctdidade4.ToString();
+                        lbl46a60por.Text = resultPorc.ToString();
+                    }
 
-                else if (int.Parse(txtIdade.Text) > 60)
-                {
-                    contadorPessoas++;
-                    ctdidade5++;
+                    else if (int.Parse(txtIdade.Text) > 60)
+                    {
+                        contadorPessoas++;
+                        ctdidade5++;
 
-                    resultPorc = (double)ctdidade5 * ((double)contadorPessoas / 100);
+                        resultPorc = (double)ctdidade5 * ((double)contadorPessoas / 100);
 
-                    lbl60mais.Text = ctdidade5.ToString();
-                    lbl60maispor.Text = resultPorc.ToString();
+                        lbl60mais.Text = ctdidade5.ToString();
+                        lbl60maispor.Text = resultPorc.ToString();
+                    }
+                    else
+                        MessageBox.Show("Número inválido", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtIdade.Clear();    
+                    txtIdade.Focus();
                 }
                 else
                     MessageBox.Show("Número inválido", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtIdade.Clear();
+                    txtIdade.Focus();
             }
-        
-            
-        } 
 
+
+        }
         private void btnFechar_Click(object sender, EventArgs e)
         {
             this.Close();
